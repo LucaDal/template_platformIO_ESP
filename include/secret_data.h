295df14661b3edd267b71172139640a697666dcb
luca <dalessandroluca1@gmail.com>
2026-04-14 22:46:12 +0200

    removed unused files

--- include/secret_data.h
+++
@@ -1,8 +0,0 @@
-#ifndef SECRET_DATA_H
-#define SECRET_DATA_H
-
-//do not include http or https protocol
-constexpr const char PORTAL_SERVER_IP[] = "ip:port/api";
-constexpr const char DEVICE_TYPE_ID[] = "TOKEN";
-
-#endif
